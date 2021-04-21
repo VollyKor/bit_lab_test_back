be58b10f189cbf10dd7914e2ea38be5efda0cac1
VolKor <vol.kor.93.@gamil.com>
2021-04-21 13:58:24 +0300

    add validation for userId and pagination

--- src/helpers/validateMiddleware.js
+++ src/helpers/validateMiddleware.js
@@ -1,7 +1,26 @@
 const Joi = require("joi").extend(require("@joi/date"));
+
 const schemaData = Joi.object().keys({
   dateFrom: Joi.date().format("YYYY-MM-DD"),
   dateTo: Joi.date().format("YYYY-MM-DD"),
+});
+
+const schemaUser = Joi.object().keys({
+  userId: Joi.string()
+    .length(10)
+    .pattern(/^[0-9]+$/)
+    .required(),
+});
+
+const schemaPagination = Joi.object().keys({
+  page: Joi.string()
+    .length(10)
+    .pattern(/^[0-9]+$/)
+    .optional(),
+  limit: Joi.string()
+    .length(2)
+    .pattern(/^[0-9]+$/)
+    .optional(),
 });
 
 const validate = (schema, obj, next) => {
@@ -15,6 +34,12 @@ const validate = (schema, obj, next) => {
   next();
 };
 
-module.exports.validate = (req, res, next) => {
+module.exports.validateDate = (req, res, next) => {
   return validate(schemaData, req.query, next);
+};
+module.exports.validateUserId = (req, res, next) => {
+  return validate(schemaUser, req.params, next);
+};
+module.exports.validatePagination = (req, res, next) => {
+  return validate(schemaPagination, req.query, next);
 };
